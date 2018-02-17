26d737e0bd75cafdc63b499e2e33b196c42977a4
Lukas Wallmann <lukas.wallmann@yahoo.de>
2018-02-17 22:45:24 +0100

    drag and drop

--- admin/modul/sites/index.php
+++ admin/modul/sites/index.php
@@ -1,7 +1,35 @@
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <style>
-   .draggable { width: 100px; height: 100px; background: #aa0000; border:1px solid;float: left; margin: 0 10px 10px 0; }
-   .itm{ height:100px; margin:20px 0; background: #ccc}
+   .draggable { width: 100px; height: 100px; background: #aa0000; border:1px solid; display:inline-block; margin: 0 10px 10px 0; }
+   .itm{ height:100px; margin:20px 0; background: #ccc;}
+   .ui-draggable-dragging{
+     z-index:9999;
+   }
+   .itm:hover .handle{
+     display: block;
+   }
+   .itm .handle{
+     display: none;
+     height: 1px;
+     background:#000;
+     cursor: pointer;
+     position: relative;
+   }
+   .itm .handle::before {
+       content: "";
+       height: 20px;
+       width: 50px;
+       background: #000;
+       display: block;
+       position: absolute;
+       left: 50%;
+       top: -20px;
+       margin-left: -25px;
+   }
+   #placeholder{
+     position: relative;
+     z-index: 99999;
+   }
   </style>
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -11,44 +39,51 @@
     $( ".draggable" ).draggable({ scroll: true, scrollSensitivity: 100, helper:"clone", drag:function(){
       checkHit($(".ui-draggable-dragging").offset())
     }, stop:function(){
-      $("#placeholder").before("<div class='itm'></div>");
-      $("#placeholder").remove();
+      $("#placeholder").replaceWith("<div class='itm'><div class='handle'></div><div class='content'>"+$(this).attr("data-type")+"</div></div>");
+      setFunctions();
     } });
+    setFunctions();
   } );
 
-  var snip=[];
+  function setFunctions(){
+    $( ".itm" ).draggable({ handle: ".handle",
+     drag:function(){checkHit($(this).offset())},
+     stop:function(){
+       $(this).css("left","auto");
+       $(this).css("top","auto");
+       $("#placeholder").replaceWith($(this));
+     }
+    });
+  }
 
   function checkHit(offset){
     snip=[];
     $("#placeholder").remove();
-    $(".itm").each(function(){
+    $(".itm:not(.ui-draggable-dragging)").each(function(){
       var off=$(this).offset();
       var h=$(this).height();
       if(off.top<offset.top+10 && off.top>offset.top-10  ){
-        $(this).css("background","#000");
-        snip=[$(this),"before"];
+        $(this).before("<div id='placeholder' style='height:2px; background:#00ff00'></div>");
       }else if(off.top+h<offset.top+10 && off.top+h>offset.top-10  ){
-        $(this).css("background","#ff0000");
-        snip=[$(this),"after"];
+        $(this).after("<div id='placeholder' style='height:2px; background:#00ff00'></div>");
       }
     });
-    if(snip!=[]){
-      if(snip[1]=="after"){
-        snip[0].after("<div id='placeholder' style='height:2px; background:#00ff00'></div>");
-      }else{
-        $(snip[0]).before("<div id='placeholder' style='height:2px; background:#00ff00'></div>");
-      }
+    if($(".itm").length==0){
+      $("#content").html("<div id='placeholder' style='height:2px; background:#00ff00'></div>");
     }
   }
 
   </script>
 
-<div class="draggable" class="ui-widget-content">
-  <p>scrollSensitivity set to 100</p>
+<div class="elements">
+<div class="draggable" data-type="text"></div>
+<div class="draggable" data-type="text-image"></div>
+<div class="draggable" data-type="image"></div>
+<div class="draggable" data-type="gallery"></div>
+<div class="draggable" data-type="downloads"></div>
+<div class="draggable" data-type="video"></div>
 </div>
 
-<div class="itm"></div>
-<div class="itm"></div>
-<div class="itm"></div>
-<div class="itm"></div>
-<div style="height: 5000px; width: 1px;"></div>
+<div id="content">
+
+</div>
