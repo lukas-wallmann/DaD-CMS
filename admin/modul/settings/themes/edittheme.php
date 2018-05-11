25e62c30553fa0808390f5bfbde9fa6de691f93c
Lukas Wallmann <lukas.wallmann@yahoo.de>
2018-05-11 20:11:35 +0200

    added ace

--- admin/modul/settings/themes/edittheme.php
+++ admin/modul/settings/themes/edittheme.php
@@ -1,9 +1,20 @@
-<div class="row">
+<div class="row h-100">
   <div class="col-md-3 leftsidebar">
     <b><?php echo $lang->themeParts ?></b><br>
     <b><?php echo $lang->themePlugins ?></b><br>
   </div>
-  <div class="col-md-9 codeEditor">
+  <div class="col-md-9 codeEditor" id="editor" style="min-height:500px">
+    function someThing(){
 
+    }
   </div>
+  <script src="js/ace/ace.js" type="text/javascript" charset="utf-8"></script>
+<script>
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/javascript");
+    editor.setOptions({
+      fontSize: "14pt"
+    })
+</script>
 </div>
