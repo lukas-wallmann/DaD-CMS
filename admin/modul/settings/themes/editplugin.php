0331d1eb6c2001eaf71a703ac9cb341a4f738e4b
Lukas Wallmann <lukas.wallmann@yahoo.de>
2018-05-22 21:50:24 +0200

    restructoring

--- admin/modul/settings/themes/editplugin.php
+++ admin/modul/settings/themes/editplugin.php
@@ -13,77 +13,13 @@
   var editor = ace.edit("editor");
   editor.setTheme("ace/theme/monokai");
   editor.session.setMode("ace/mode/javascript");
+  editor.session.setUseWorker(false);
   editor.setOptions({
     fontSize: "13pt"
   });
   var pluginID=<?php echo $_GET["ID"] ?>;
 </script>
-<script>
-  $(document).ready(function(){
-    $.getJSON("?m=settings/themes&f=apiplugins&no=1&ID="+pluginID, function( data ) {
-      pluginEditor.init(data);
-    });
-  });
-  var pluginEditor={
-
-    currentMode:"",
-    cache:{},
-
-    init:function(code){
-      pluginEditor.cache=code;
-      $(".leftsidebar .plugincode").click(pluginEditor.getPluginCode);
-      $(".leftsidebar .template").click(pluginEditor.getTemplateCode);
-      $(".leftsidebar .save").click(pluginEditor.save);
-      $(document).bind('keydown', function(e) {
-        if(e.ctrlKey && (e.which == 83)) {
-          e.preventDefault();
-          pluginEditor.save();
-          return false;
-        }
-      });
-      pluginEditor.getPluginCode();
-    },
-
-    getPluginCode:function(){
-      $(".selected").removeClass("selected");
-      $(".leftsidebar .plugincode").addClass("selected");
-      pluginEditor.saveTemp();
-      pluginEditor.currentMode="plugin";
-      editor.session.setMode("ace/mode/javascript");
-      editor.setValue(pluginEditor.cache.code);
-    },
-
-    getTemplateCode:function(){
-      $(".selected").removeClass("selected");
-      $(".leftsidebar .template").addClass("selected");
-      pluginEditor.saveTemp();
-      pluginEditor.currentMode="template";
-      editor.session.setMode("ace/mode/html");
-      editor.setValue(pluginEditor.cache.template);
-    },
-
-    saveTemp:function(){
-      if(pluginEditor.currentMode=="plugin"){
-        pluginEditor.cache.code=editor.getValue();
-      }else if(pluginEditor.currentMode=="template"){
-        pluginEditor.cache.template=editor.getValue();
-      }
-    },
-
-    save:function(){
-      pluginEditor.saveTemp();
-      var data={};
-      data.cache=JSON.stringify(pluginEditor.cache);
-      $.ajax({
-        type: "POST",
-        url: "?m=settings/themes&f=apiplugins&no=1&ID="+pluginID,
-        data: data
-      }).done(function(){
-        alert("done");
-      });
-    }
-  }
-</script>
+<script src="js/pluginEditor.js"></script>
 <style>
   .selected{
     font-style: italic;
