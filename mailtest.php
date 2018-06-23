d8fd50726bf50001bfaff055ab1bfbab2d70aeed
Lukas Wallmann <lukas.wallmann@yahoo.de>
2018-06-23 17:11:19 +0200

    testing phpmailer

--- mailtest.php
+++ mailtest.php
@@ -1,7 +1,16 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'includes/phpmailer/src/Exception.php';
+require 'includes/phpmailer/src/PHPMailer.php';
+require 'includes/phpmailer/src/SMTP.php';
 
 // mehrere Empfänger
-$empfaenger  = 'lukas@wallmanns-ideenwerkstatt.com, john.doe.jagg@gmail.com, lukas.wallmann@yahoo.de'; // beachte das Komma
+$empfaenger  = 'lukas@wallmanns-ideenwerkstatt.com'; // beachte das Komma
 
 // Betreff
 $betreff = 'Testmail 123 äöü';
@@ -30,19 +39,41 @@ $nachricht = '
 </html>
 ';
 
-utf8mail($empfaenger,$betreff,$nachricht);
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
 
-function utf8mail($to,$s,$body,$from_name="öäü",$from_a = "office@wallmanns-ideenwerkstatt.com", $reply="office@wallmanns-ideenwerkstatt.com")
-{
-    $s= "=?utf-8?b?".base64_encode($s)."?=";
-    $headers = "MIME-Version: 1.0\r\n";
-    $headers.= "From: =?utf-8?b?".base64_encode($from_name)."?= <".$from_a.">\r\n";
-    $headers.= "Content-Type: text/html;charset=utf-8\r\n";
-    $headers.= "Reply-To: $reply\r\n";
-    $headers.= "X-Mailer: PHP/" . phpversion();
-    echo $headers;
-    mail($to, $s, $body, $headers);
+    $mail->CharSet = 'UTF-8';
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.mail.yahoo.com';
+    // use
+    // $mail->Host = gethostbyname('smtp.gmail.com');
+    // if your network does not support SMTP over IPv6
+    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+    $mail->Port = 587;
+    //Set the encryption system to use - ssl (deprecated) or tls
+    $mail->SMTPSecure = 'tls';
+    //Whether to use SMTP authentication
+    $mail->SMTPAuth = true;
+    //Username to use for SMTP authentication - use full email address for gmail
+    $mail->Username = "lukas.wallmann@yahoo.de";
+    //Password to use for SMTP authentication
+    $mail->Password = "********";
+    //Recipients
+    $mail->setFrom('lukas.wallmann@yahoo.de', 'John Doe JaGG');
+    $mail->addAddress($empfaenger);     // Add a recipient
+    $mail->addReplyTo('john.doe.jagg@gmail.com', 'John Doe JaGG');
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $betreff;
+    $mail->Body    = $nachricht;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
 }
-
 
  ?>
